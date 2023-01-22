d3c8b40945c871c6589fdb1a7659bf00af541357
Sandeep Raghuraman <sandy.8925@gmail.com>
2023-01-22 23:22:19 +0530

    HelpActivity: Change to load file contents and do HTML parsing on a background thread

--- android/src/main/java/org/ligi/blexplorer/HelpActivity.kt
+++ android/src/main/java/org/ligi/blexplorer/HelpActivity.kt
@@ -1,39 +1,49 @@
 package org.ligi.blexplorer
 
 import android.os.Bundle
-import android.text.Html
 import android.text.method.LinkMovementMethod
 import android.view.MenuItem
+import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
+import com.uber.autodispose.AutoDispose
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider.from
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import org.ligi.blexplorer.databinding.ActivityWithTextviewBinding
 import org.ligi.compat.HtmlCompat
-import java.io.IOException
+import timber.log.Timber
 
 class HelpActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityWithTextviewBinding
+    private val binding by lazy { ActivityWithTextviewBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-        binding = ActivityWithTextviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        loadHelpText()
+    }
 
-        try {
-            val open = assets.open("help.html")
+    @MainThread private fun loadHelpText() {
+        Single.fromCallable {
+            val launcherDrawable = ContextCompat.getDrawable(this, R.drawable.ic_launcher)
+                ?.apply { setBounds(0, 0, intrinsicWidth, intrinsicHeight) }
+            val inputStream = assets.open("help.html")
+            HtmlCompat.fromHtml(
+                inputStream.bufferedReader().readText(),
+                { launcherDrawable },
+                null
+            )
+        }.subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnSuccess {
             binding.contentText.movementMethod = LinkMovementMethod.getInstance()
-            binding.contentText.text = HtmlCompat.fromHtml(open.bufferedReader().readText(), Html.ImageGetter {
-                ContextCompat.getDrawable(this, R.drawable.ic_launcher)?.apply {
-                    setBounds(0, 0, intrinsicWidth, intrinsicHeight)
-                }
-            }, null)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-
+            binding.contentText.text = it
+        }.doOnError { Timber.e(it, "Failed to load help text") }
+        .`as`(AutoDispose.autoDisposable(from(this, ON_DESTROY)))
+        .subscribe({},{})
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
