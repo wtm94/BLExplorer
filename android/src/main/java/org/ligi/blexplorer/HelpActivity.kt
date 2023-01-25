fb9c8122c4a4ddf7dfb81df4fc842ec2412feac0
Sandeep Raghuraman <sandy.8925@gmail.com>
2023-01-25 18:38:04 +0530

    HelpActivity: Move the help text loading code into a LiveData

--- android/src/main/java/org/ligi/blexplorer/HelpActivity.kt
+++ android/src/main/java/org/ligi/blexplorer/HelpActivity.kt
@@ -1,24 +1,27 @@
 package org.ligi.blexplorer
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.MenuItem
 import androidx.activity.viewModels
-import androidx.annotation.MainThread
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import com.uber.autodispose.AutoDispose.autoDisposable
-import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider.from
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.ligi.blexplorer.databinding.ActivityWithTextviewBinding
 import org.ligi.compat.HtmlCompat
 import timber.log.Timber
+import javax.inject.Inject
 
-class HelpActivity : AppCompatActivity() {
+@AndroidEntryPoint class HelpActivity : AppCompatActivity() {
     private val binding by lazy { ActivityWithTextviewBinding.inflate(layoutInflater) }
     private val viewModel: HelpViewModel by viewModels()
 
@@ -26,31 +29,9 @@ class HelpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         setContentView(binding.root)
+
         binding.contentText.movementMethod = LinkMovementMethod.getInstance()
-
-        viewModel.loadedHtml?.let { binding.contentText.text = it }
-        viewModel.loadedHtml ?: loadHelpText()
-    }
-
-    @MainThread private fun loadHelpText() {
-        Single.fromCallable {
-            val launcherDrawable = ContextCompat.getDrawable(this, R.drawable.ic_launcher)
-                ?.apply { setBounds(0, 0, intrinsicWidth, intrinsicHeight) }
-            val inputStream = assets.open("help.html")
-            HtmlCompat.fromHtml(
-                inputStream.bufferedReader().readText(),
-                { launcherDrawable },
-                null
-            )
-        }.subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .doOnSuccess {
-            viewModel.loadedHtml = it
-            binding.contentText.text = it
-        }
-        .doOnError { Timber.e(it, "Failed to load help text") }
-        .`as`(autoDisposable(from(this, ON_DESTROY)))
-        .subscribe({},{})
+        viewModel.helpText.observe(this) { binding.contentText.text = it }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -59,6 +40,26 @@ class HelpActivity : AppCompatActivity() {
     }
 }
 
-class HelpViewModel : ViewModel() {
-    var loadedHtml: CharSequence? = null
+@HiltViewModel class HelpViewModel @Inject constructor(@ApplicationContext context: Context) : ViewModel() {
+    internal val helpText: LiveData<CharSequence> = HelpTextLiveData(context)
+}
+
+@SuppressLint("CheckResult")
+private class HelpTextLiveData(context: Context) : LiveData<CharSequence>() {
+    init {
+        Single.fromCallable {
+            val launcherDrawable = ContextCompat.getDrawable(context, R.drawable.ic_launcher)
+                ?.apply { setBounds(0, 0, intrinsicWidth, intrinsicHeight) }
+            val inputStream = context.assets.open("help.html")
+            HtmlCompat.fromHtml(
+                inputStream.bufferedReader().readText(),
+                { launcherDrawable },
+                null
+            )
+        }.subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .doOnSuccess { value = it }
+        .doOnError { Timber.e(it, "Failed to load help text") }
+        .subscribe({},{})
+    }
 }
