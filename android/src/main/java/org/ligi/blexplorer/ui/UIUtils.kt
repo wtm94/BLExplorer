a24269f386041f060743290314f32a71c0ff250d
Sandeep Raghuraman <sandy.8925@gmail.com>
2023-01-25 20:01:48 +0530

    Code cleanup

--- android/src/main/java/org/ligi/blexplorer/ui/UIUtils.kt
+++ android/src/main/java/org/ligi/blexplorer/ui/UIUtils.kt
@@ -4,11 +4,11 @@ import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.Event.ON_DESTROY
 import androidx.recyclerview.widget.RecyclerView
 import com.polidea.rxandroidble2.RxBleClient
-import com.uber.autodispose.AutoDispose
-import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider
+import com.uber.autodispose.AutoDispose.autoDisposable
+import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider.from
 import io.reactivex.BackpressureStrategy
 import io.reactivex.android.schedulers.AndroidSchedulers
 import org.ligi.blexplorer.bluetoothController
@@ -18,13 +18,12 @@ internal const val BUG_REPORT_EMAIL = "sandy.8925@gmail.com"
 class ExitActivityOnBluetoothDisable : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val autoDisposable = AutoDispose.autoDisposable<RxBleClient.State>(AndroidLifecycleScopeProvider.from(this, Lifecycle.Event.ON_DESTROY))
         bluetoothController.bluetoothStateEvents
                 .toFlowable(BackpressureStrategy.LATEST)
                 .filter { it == RxBleClient.State.BLUETOOTH_NOT_ENABLED }
                 .take(1)
                 .observeOn(AndroidSchedulers.mainThread())
-                .`as`(autoDisposable)
+                .`as`(autoDisposable(from(this, ON_DESTROY)))
                 .subscribe({ activity?.finish() } , {})
     }
 }
